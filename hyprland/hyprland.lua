215d84347d66459ce43bf557c602ba82a95f63bf
Pandemmoniumm <elliotgorven@gmail.com>
2026-09-25 09:54:17 +0200

    Update and rename Arch/hyprland/hyprland.lua to hyprland/hyprland.lua

--- hyprland/hyprland.lua
+++ hyprland/hyprland.lua
@@ -11,5 +11,3 @@ require("autostart")
 colors = require("dark")
 base = colors.base
 surface0_ee = "rgba(" .. colors.surface0Alpha .. "ee)"
--- For Noctalia Color templates
-require("noctalia").apply_theme()
