e2584bdd6b43f4fa7e51fbd92e792bde9e973ee0
Pandemmoniumm <elliotgorven@gmail.com>
2026-09-25 09:55:01 +0200

    Update sensitivity setting in input.lua
    Set sensitivity to 0, indicating no modification.

--- hyprland/input.lua
+++ hyprland/input.lua
@@ -8,7 +8,7 @@ hl.config({
 
         follow_mouse = 1,
 
-        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+        sensitivity = 0,
 
         touchpad = {
             natural_scroll = true,
